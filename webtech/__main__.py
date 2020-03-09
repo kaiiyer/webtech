a50212a8332f684bc7d3922e802b59bedca06d75
Aayush Upadhyay <upadhyayaayush12@gmail.com>
2020-03-09 05:52:06 +0000

    Update __main__.py

--- webtech/__main__.py
+++ webtech/__main__.py
@@ -2,8 +2,8 @@
 import sys
 from optparse import OptionParser
 
-from __version__ import __version__ as VERSION
-from webtech import WebTech
+from .__version__ import __version__ as VERSION
+from .webtech import WebTech
 
 
 def split_on_comma(option, opt_str, value, parser):

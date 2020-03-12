bf54e2d02c9cde08be5ca75628f549dbffa6c0e9
Aayush Upadhyay <upadhyayaayush12@gmail.com>
2020-03-12 11:50:37 +0530

    Fixed module not found error (#49)
    * Update __main__.py
    
    * Fixed module not found error
    
    * Update __main__.py
    
    * Update __main__.py
    
    Co-authored-by: Kai Iyer <24914913+kaiiyer@users.noreply.github.com>

--- webtech/__main__.py
+++ webtech/__main__.py
@@ -46,24 +46,19 @@ def main():
     (options, _args) = parser.parse_args(sys.argv)
     options = vars(options)
 
-    if options.get('urls') is None and options.get('urls_file') is None and not options.get('update_db'):
-        print("No URL(s) given!")
-        parser.print_help()
-        exit()
-
     wt = WebTech(options)
     if options.get('scrape'):
-
         """
         Bad style of making arguments mutually exclusive.
         Use Argparse's mutually exclusive groups.
         """
-
         wt.scraping()
 
     else:
+        if options.get('urls') is None and options.get('urls_file') is None and not options.get('update_db'):
+            print("No URL(s) given!")
+            parser.print_help()
+            exit()
         wt.start()
-
-
 if __name__ == "__main__":
     main()

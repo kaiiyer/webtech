88e5b42e6d714fd5f619acaff1fae6caa3ab4f39
ashwinijha6 <ashwinijha006@gmail.com>
2020-06-01 11:59:02 +0530

    Web Scraping Dynamic websites usingscrapy

---
+++ scrapy/items.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+from scrapy.item import Item, Field
+
+class StackItem(Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    pass
